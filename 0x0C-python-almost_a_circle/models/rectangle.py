da2abecf87a0dbe9ceb624ba566cbdaf8755309b
kmlsd <kmlsd4757@gmail.com>
2024-02-26 17:50:22 +0000

    frist rectang

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This is the Rectangle module.
+
+Contains the Rectangle class that inherits from Base.
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """This class inherits from Base and defines a Rectangle object.
+
+    Attributes:
+        __width (int): the width of the rectangle.
+        __height (int): the height of the rectangle.
+        __x (int): the horizontal (x) padding of the rectangle.
+        __y (int): the vertical (y) padding of the rectangle.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes the default attributes of the Base object.
+
+        Args:
+            width (int): the wanted width of the rectangle.
+            height (int): the wanted height of the rectangle.
+            x (int): the wanted horizontal (x) padding of the rectangle.
+            y (int): the wanted vertical (y) padding of the rectangle.
+            id (int): the wanted identifier of the Base object.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
