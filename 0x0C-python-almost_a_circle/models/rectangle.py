b77736db964db3de3d5bbc5a96f7073cdf53afa0
kmlsd <kmlsd4757@gmail.com>
2024-02-29 15:04:48 +0000

    frist recta

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,66 +1,54 @@
 #!/usr/bin/python3
-"""This is the Rectangle module.
-
-Contains the Rectangle class that inherits from Base.
-"""
+'''Module for Rectangle class.'''
 from models.base import Base
 
 
 class Rectangle(Base):
-    """This class inherits from Base and defines a Rectangle object.
+    '''A Rectangle class.'''
 
-    Attributes:
-        __width (int): the width of the rectangle.
-        __height (int): the height of the rectangle.
-        __x (int): the horizontal (x) padding of the rectangle.
-        __y (int): the vertical (y) padding of the rectangle.
-    """
-
-	def __init__(self, width, height, x=0, y=0, id=None):
-        """Initializes the default attributes of the Base object.
-
-        Args:
-            width (int): the wanted width of the rectangle.
-            height (int): the wanted height of the rectangle.
-            x (int): the wanted horizontal (x) padding of the rectangle.
-            y (int): the wanted vertical (y) padding of the rectangle.
-            id (int): the wanted identifier of the Base object.
-        """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        '''Constructor.'''
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+    @property
+    def width(self):
+        '''Width of this rectangle.'''
+        return self.__width
 
-    
-	@property
-	def width(self):
-		return self.__width
+    @width.setter
+    def width(self, value):
+        self.validate_integer("width", value, False)
+        self.__width = value
 
-	@width.setter
-	def width(self, value):
-        	self.__width = value
+    @property
+    def height(self):
+        '''Height of this rectangle.'''
+        return self.__height
 
-	@property
-	def height(self):
-		return self.__height
+    @height.setter
+    def height(self, value):
+        self.validate_integer("height", value, False)
+        self.__height = value
 
-	@height.setter
-	def height(self, value):
-        	self.__height = value
+    @property
+    def x(self):
+        '''x of this rectangle.'''
+        return self.__x
 
-	@property
-	def x(self):
-		return self.__x
+    @x.setter
+    def x(self, value):
+        self.validate_integer("x", value)
+        self.__x = value
 
-	@x.setter
-	def x(self, value):
-		self.__x = value
+    @property
+    def y(self):
+        '''y of this rectangle.'''
+        return self.__y
 
-	@property
-	def y(self):
-		return self.__y
-
-	@y.setter
-	def y(self, value):
-		self.__y = value
+    @y.setter
+    def y(self, value):
+        self.validate_integer("y", value)
+        self.__y = value
