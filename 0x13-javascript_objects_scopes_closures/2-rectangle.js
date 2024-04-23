78cdd022e250255077c3142ecb7fcffab730b71d
kmlsd <kmlsd4757@gmail.com>
2024-04-23 21:54:06 +0000

    2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,8 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
+     if ((w = parseInt(w)) > 0 &&
+        (h = parseInt(h)) > 0) {
       this.width = w;
       this.height = h;
     }
