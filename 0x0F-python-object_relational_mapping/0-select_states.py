3e7ad321022d0c1b410eca10ad2b7f794ca0853b
kmlsd <kmlsd4757@gmail.com>
2024-04-28 20:09:34 +0000

    0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Script that lists all `states` from the database `hbtn_0e_0_usa`.
+
+Arguments:
+    mysql username (str)
+    mysql password (str)
+    database 
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    mySQL_u = sys.argv[1]
+    mySQL_p = sys.argv[2]
+    db_name = sys.argv[3]
+
+dbconfig = { 'host': '127.0.0.1', 'port':'3306','user':'mySQL_u', 'passwd':'mySQL_p', 'db' :'db_name'}
+
+conn = MySQLdb.connect(**dbconfig)
+ cur = conn.cursor()
+sql_code = """SELECT * FROM states ORDER BY id"""
+
+    cur.execute(sql_code)
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
