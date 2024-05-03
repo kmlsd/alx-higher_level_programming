d819c8878e9adcc72eda501cf9f74b95d3404318
kmlsd <kmlsd4757@gmail.com>
2024-05-03 19:08:16 +0000

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,7 +3,7 @@
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import sessionmaker
 from model_city import City
 
 Base = declarative_base()
