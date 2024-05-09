2d23d0d8aed37230212df6ffe93e50bf17c0517f
kmlsd <kmlsd4757@gmail.com>
2024-05-09 21:05:44 +0000

    query string

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,5 @@
 
 #!/bin/bash
 # Displays the body of the response of a curl POST request
-curl -s  -X POST -d "email=test@gmail.com"&"subject=I will always be here for PLD" "$1"
+curl -X "POST" -s "email=test@gmail.com"&"subject=I will always be here for PLD" "$1"
 
