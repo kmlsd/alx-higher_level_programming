77a7f6f7ebb48ccfcaad1e1c10b37064aa8be4be
kmlsd <kmlsd4757@gmail.com>
2024-05-10 23:55:44 +0000

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(type(r.text))
+    print("\t- content: {}".format(r.text))
 
