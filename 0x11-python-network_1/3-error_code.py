7e0d23ef3665f52305698e39002421626f7a25e3
kmlsd <kmlsd4757@gmail.com>
2024-05-10 21:02:13 +0000

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,22 +1,14 @@
 
 #!/usr/bin/python3
-"""Sends a request to a given URL and displays the response body.
+"""Python script that takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8)."""
 
-Usage: ./3-error_code.py <URL>
-  - Handles HTTP errors.
-"""
-import sys
-import urllib.error
-import urllib.request
-
+from urllib import request, error
+from sys import argv
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    try:
-        with urllib.request.urlopen(request) as response:
-            print(response.read().decode("ascii"))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
-
+        try:
+            with request.urlopen(argv[1]) as page:
+                print(page.read().decode('utf-8'))
+        except error.HTTPError as e:
+            print("Error code: {}".format(e.code))
