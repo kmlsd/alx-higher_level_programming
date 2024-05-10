456c911ec0b24786f33aade6d97e95684077d10c
kmlsd <kmlsd4757@gmail.com>
2024-05-10 22:22:22 +0000

    10-my_github.p

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,13 +1,13 @@
-
 #!/usr/bin/python3
-"""Python script that takes GitHub credentials (username and
-password) and uses the GitHub API to display the user id."""
-
+""" Uses requests module. Prints error code"""
 import requests
 from sys import argv
 
 if __name__ == "__main__":
+    url = "https://api.github.com/user"
     auth = (argv[1], argv[2])
-    request = requests.get("https://api.github.com/user", auth=auth)
-    print(request.json().get("id"))
-
+    response = requests.get(url, auth=auth)
+    try:
+        print(response.json().get("id"))
+    except ValueError:
+        print("Not a valid JSON")
