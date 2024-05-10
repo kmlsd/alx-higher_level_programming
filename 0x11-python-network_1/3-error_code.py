7a73ea109713ab2ffc5611cd0bd4a3ac100383bf
kmlsd <kmlsd4757@gmail.com>
2024-05-10 23:38:39 +0000

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,19 @@
-
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL
-and displays the body of the response (decoded in utf-8)."""
+"""Python script that takes in a URL,
+sends a request to the URL and displays the body
+of the response (decoded in utf-8).
+"""
+import sys
+import urllib.error
+import urllib.request
 
-from urllib import request, error
-from sys import argv
 
 if __name__ == "__main__":
-        try:
-            with request.urlopen(argv[1]) as page:
-                print(page.read().decode('utf-8'))
-        except error.HTTPError as e:
-            print("Error code: {}".format(e.code))
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
