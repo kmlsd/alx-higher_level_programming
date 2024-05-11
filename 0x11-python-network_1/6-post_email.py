2ed6877b002c7e35bd168bd71f9e18337dd44017
kmlsd <kmlsd4757@gmail.com>
2024-05-11 00:15:13 +0000

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
-
-Usage: ./6-post_email.py <URL> <email>
-  - Displays the body of the response.
+"""Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a
+parameter, and finally displays the body of the response.
 """
-import sys
-import requests
-
-
 if __name__ == "__main__":
+    import requests
+    import sys
+
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
+    email = sys.argv[2]
+    data_req = requests.post(url, {"email": email})
 
-    r = requests.post(url, data=value)
-    print(r.text)
-
+    print(data_req.text)
