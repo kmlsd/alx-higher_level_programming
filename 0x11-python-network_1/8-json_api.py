87c8d373a9cbde602b5f6de84588e651740466b6
kmlsd <kmlsd4757@gmail.com>
2024-05-11 00:18:35 +0000

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,24 +1,23 @@
-
 #!/usr/bin/python3
-"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
-
-Usage: ./8-json_api.py <letter>
-  - The letter is sent as the value of the variable `q`.
-  - If no letter is provided, sends `q=""`.
+"""Python script that takes in a letter and sends a
+POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
 """
+import sys
 import requests
-from sys import argv
+
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5000/search_user"
-    data = {"q": argv[1][0] if len(argv) > 1 else ""}
-    response = requests.post(url, data=data)
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    m = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        d = response.json()
-        if not d:
+        response = m.json()
+        if response == {}:
             print("No result")
         else:
-            print("[{}] {}".format(d.get("id"), d.get("name")))
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
         print("Not a valid JSON")
 
