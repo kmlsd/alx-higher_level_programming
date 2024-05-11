6ed0207350654778d5aadea4f0e481a8586b59a6
kmlsd <kmlsd4757@gmail.com>
2024-05-11 00:35:27 +0000

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,7 @@
-
 #!/usr/bin/python3
-"""Python script that takes in a URL,
-sends a request to the URL and displays
-the value of the variable X-Request-Id in the response header
+"""Displays the X-Request-Id header variable of a request to a given URL.
+
+Usage: ./5-hbtn_header.py <URL>
 """
 import sys
 import requests
@@ -11,7 +10,5 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    m = requests.get(url)
-    print(m.headers.get("X-Request-Id"))
-
-
+    r = requests.get(url)
+    print(r.headers.get("X-Request-Id"))
