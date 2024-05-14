ddd9630185438db629efbc7ae3e4ea0e3ca6f62c
kmlsd <kmlsd4757@gmail.com>
2024-05-14 20:55:36 +0000

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
 """
-Module using request that fetches https://intranet.hbtn.io/status
+Write a Python script that fetches https://alx-intranet.hbtn.io/status
 """
 import requests
 
 
-def main():
-    """
-    Function that fetches https://intranet.hbtn.io/status
-    """
-    url = 'https://intranet.hbtn.io/status'
-    r = requests.get(url)
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
-
-if __name__ == "__main__":
-    main()
-
