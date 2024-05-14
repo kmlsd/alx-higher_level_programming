95155e3492c19b18a762cc0a6e3a3651b051f6b3
kmlsd <kmlsd4757@gmail.com>
2024-05-14 18:18:14 +0000

    send delet

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # Bash script that sends a DELETE request to the URL passed
-curl -sX DELETE "$1"
+#curl -sX DELETE "$1"
+URL=$1
 
+reply=$(curl -X DELETE -s -w "%{http_code}" $URL)
+http_reply_status=${reply:(-3)}
+reply_body=${reply:0:${#reply}-3}
+echo "I'm a DELETE request$reply_body"
