95432e361b001c8442ce5260b144441fe5cd9f47
kmlsd <kmlsd4757@gmail.com>
2024-05-14 20:52:50 +0000

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -11,9 +11,9 @@ def main():
     """
     url = 'https://intranet.hbtn.io/status'
     r = requests.get(url)
-    print('Body response:')
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
 
 if __name__ == "__main__":
     main()
