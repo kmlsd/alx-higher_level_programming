a7303e546cbcc2ae5894e3e41ab6abf089dab243
kmlsd <kmlsd4757@gmail.com>
2024-05-14 18:38:39 +0000

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#Bash script that takes in a URL, sends a POST request to the passed URL
-curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
+#Bash script that takes in a URL, sends a POST request to the passed UR
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I+will+always+be+here+for+PLD"
 
