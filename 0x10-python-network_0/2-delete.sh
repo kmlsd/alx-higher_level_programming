d63301440bf72cfa2ffd31574036092b0373dd37
kmlsd <kmlsd4757@gmail.com>
2024-05-14 18:24:01 +0000

    send delet

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -6,4 +6,4 @@ URL=$1
 reply=$(curl -X DELETE -s -w "%{http_code}" $URL)
 http_reply_status=${reply:(-3)}
 reply_body=${reply:0:${#reply}-3}
-echo "I'm a DELETE request$reply_body"
+echo "I'm a DELETE request$reply"
