00f4c1051f399e41317a20f7125f621a1e4ea921
kmlsd <kmlsd4757@gmail.com>
2024-06-17 14:10:04 -0700

    7-script.js

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+
+$.getJSON(url, function (data) {
+  $('DIV#character').text(data.name);
+});
+});
