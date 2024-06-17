97bea2348d0de2ad27aae587f6171a94c902d842
kmlsd <kmlsd4757@gmail.com>
2024-06-17 14:15:37 -0700

    8-script.js

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+
+$.getJSON(url, function (data) {
+  $.each(data.results, function (i, movie) {
+    $('UL#list_movies').append('<li>' + movie.title + '</li>');
+  });
+});
+});
